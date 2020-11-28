83bc31a82f92a62ca952d341eaa06f9a16dd63a2
kennyigbechi <kennyigbechi@gmail.com>
2020-11-28 07:32:03 -0800

    Complete Users and WebSites

--- Problems/Users and WebSites/src/Main.java
+++ Problems/Users and WebSites/src/Main.java
@@ -1,23 +1,41 @@
-class User {
 
+abstract class BaseEntity {
     protected long id;
-
     protected long version;
+
+
+    public abstract long getId();
+
+    public abstract void setId(long id);
+
+    public abstract long getVersion();
+
+    public abstract void setVersion(long version);
+
+}
+
+
+class User extends BaseEntity {
 
     protected String name;
 
+
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public void setId(long id) {
         this.id = id;
     }
 
+    @Override
     public long getVersion() {
         return version;
     }
 
+    @Override
     public void setVersion(long version) {
         this.version = version;
     }
@@ -29,33 +47,38 @@ class User {
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
 
-class Visit {
+class Visit extends BaseEntity {
 
-    protected long id;
-
-    protected long version;
 
     protected User user;
 
     protected WebSite site;
 
+
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public void setId(long id) {
         this.id = id;
     }
 
+    @Override
     public long getVersion() {
         return version;
     }
 
+    @Override
     public void setVersion(long version) {
         this.version = version;
     }
+
 
     public User getUser() {
         return user;
@@ -74,29 +97,31 @@ class Visit {
     }
 }
 
-class WebSite {
+class WebSite extends BaseEntity {
 
-    protected long id;
-
-    protected long version;
 
     protected String url;
 
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public void setId(long id) {
         this.id = id;
     }
 
+    @Override
     public long getVersion() {
         return version;
     }
 
+    @Override
     public void setVersion(long version) {
         this.version = version;
     }
+
 
     public String getUrl() {
         return url;
@@ -106,3 +131,4 @@ class WebSite {
         this.url = url;
     }
 }
+
